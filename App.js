9ecdf5fd2b2b2cfda8c7c75875d54c877b6ec8e1
Venkata Naidu <Chowdaryrk077@gmail.com>
2024-08-27 07:46:35 -0400

    TypeScript to JavaScript

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, ScrollView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Text> Hello </Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
